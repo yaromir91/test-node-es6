551a15a4227a31a1ced5076c948a63e41a381e11
Yaromir Kozak <yaromir.artjoker@gmail.com>
2016-07-28 13:24:24 +0300

    create test for reviews

--- server/routes/v1/review.js
+++ server/routes/v1/review.js
@@ -2,7 +2,6 @@ import express from 'express';
 import validate from 'express-validation';
 import paramValidation from '../../config/param-validation';
 import reviewCtrl from '../../controllers/review';
-import productCtrl from '../../controllers/product';
 
 const v1 = express.Router();	// eslint-disable-line new-cap
 
@@ -14,13 +13,8 @@ v1.route('/:reviewId')
   /** GET /v1/reviews/:reviewId - Get review */
   .get(reviewCtrl.get)
 
-  /** DELETE /v1/reviews/:reviewId - Delete review */
-  .delete(reviewCtrl.remove);
 
 
-
-/** Load product when API with productId route parameter is hit */
-v1.param('productId', productCtrl.load);
 /** Load review when API with reviewId route parameter is hit */
 v1.param('reviewId', reviewCtrl.load);
 
