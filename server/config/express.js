b76146b55e446cade4158146264aa9c1ac92c89a
Yaromir Kozak <yaromir.artjoker@gmail.com>
2016-08-11 14:38:01 +0300

    small_fix

--- server/config/express.js
+++ server/config/express.js
@@ -53,7 +53,6 @@ app.use('/', routes);
 
 // if error is not an instanceOf APIError, convert it.
 app.use((err, req, res, next) => {
-  console.log(err)
   if (err instanceof expressValidation.ValidationError) {
     // validation error contains errors which is an array of error each containing message[]
     const unifiedErrorMessage = err.errors.map(error => error.messages.join('. ')).join(' and ');
--- server/models/user.statics.js
+++ server/models/user.statics.js
@@ -38,7 +38,7 @@ export default {
                 }
                 return errorUserExists;
             }).catch(function () {
-                return error;
+                return errorUserExists;
             });
     },
     
